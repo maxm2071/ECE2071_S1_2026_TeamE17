897fbc95163c2c764a6163b13508d9656137436a
maxm2071 <mall0045@student.monash.edu>
2026-04-28 13:12:10 +1000

    Organised Project Files
    Organised project files for Task 1, added python script and moving average filter for the processing STM

---
+++ ProjectTask1/PythonScript.py
@@ -0,0 +1,33 @@
+import serial
+import serial.tools.list_ports
+import numpy as np
+import wave
+
+devices = serial.tools.list_ports.comports()
+
+for i in devices:
+    print(i)
+
+ser = serial.Serial("COM3", 115200)
+
+my_list = []
+
+SAMPLE_RATE = 10000
+
+for i in range(5 * SAMPLE_RATE):
+    byte = ser.read(1)
+    bit = byte[0]
+    my_list.append(bit)
+
+data = np.array(my_list)
+print(data)
+
+data = (data - data.min()) / (data.max() - data.min())
+data = data * 255
+data = data.astype(np.uint8)
+
+with wave.open('W7_Practical.wav', 'wb') as wf:
+    wf.setnchannels(1)
+    wf.setsampwidth(1)
+    wf.setframerate(SAMPLE_RATE)
+    wf.writeframes(data.tobytes())
