328a94d279e87d2354a1a0b933e2c0e2727a580d
yvanugon-debug <yvan.ugon@gmail.com>
2026-05-03 15:33:43 +1000

    created python UI
    created python UI, sample rate at 22.05ksps, creates csv, user inputted length wav file, saves png of audio track

--- ProjectTask1/PythonScript.py
+++ ProjectTask1/PythonScript.py
@@ -1,5 +1,6 @@
 import serial
 import serial.tools.list_ports
+import matplotlib.pyplot as plt
 import numpy as np
 import wave
 
@@ -8,19 +9,19 @@ devices = serial.tools.list_ports.comports()
 for i in devices:
     print(i)
 
-ser = serial.Serial("COM3", 115200)
 
 my_list = []
 
-SAMPLE_RATE = 5000
+SAMPLE_RATE = 22050
+clip_length = int(input("Enter the clip length: "))
 
-for i in range(10 * SAMPLE_RATE):
+for i in range(clip_length * SAMPLE_RATE):
     byte = ser.read(1)
     bit = byte[0]
     my_list.append(bit)
 
 data = np.array(my_list)
-print(data)
+print(len(data))
 
 data = (data - data.min()) / (data.max() - data.min())
 data = data * 255
@@ -31,5 +32,15 @@ with wave.open('Project_Task_1.wav', 'wb') as wf:
     wf.setsampwidth(1)
     wf.setframerate(SAMPLE_RATE)
     wf.writeframes(data.tobytes())
+array = np.arange(0,clip_length,1/SAMPLE_RATE)
+plt.plot(array,data)
+plt.ylabel('Amplitude')
+plt.xlabel('Time (s)')
+plt.title('amplitude vs time of audio')
+plt.savefig('Project_Task_1.png')
 
+csv = np.insert(array,0,SAMPLE_RATE)
+np.savetxt('Project_Task_1.csv',csv,delimiter=',')
+
+plt.show()
 print("done")
