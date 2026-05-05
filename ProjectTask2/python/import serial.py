ea40cb2ed4f25b606d1028a721b6f73973afb45c
yvanugon-debug <yvan.ugon@gmail.com>
2026-05-05 22:54:50 +1000

    task2 start
    only transmits when US sensor reads less than 10cm

---
+++ ProjectTask2/python/import serial.py
@@ -0,0 +1,50 @@
+import serial
+import serial.tools.list_ports
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+
+devices = serial.tools.list_ports.comports()
+
+for i in devices:
+    print(i)
+
+ser = serial.Serial("COM10", 115200)
+
+ser.write('1'.encode())
+
+my_list = []
+
+SAMPLE_RATE = 5000
+clip_length = 10
+
+for i in range(10 * SAMPLE_RATE):
+    byte = ser.read(1)
+    bit = byte[0]
+    my_list.append(bit)
+
+data = np.array(my_list)
+print(data)
+
+data = (data - data.min()) / (data.max() - data.min())
+data = data * 255
+data = data.astype(np.uint8)
+
+with wave.open('Project_Task_1.wav', 'wb') as wf:
+    wf.setnchannels(1)
+    wf.setsampwidth(1)
+    wf.setframerate(SAMPLE_RATE)
+    wf.writeframes(data.tobytes())
+
+array = np.arange(0,clip_length,1/SAMPLE_RATE)
+plt.plot(array,data)
+plt.ylabel('Amplitude')
+plt.xlabel('Time (s)')
+plt.title('amplitude vs time of audio')
+plt.savefig('Project_Task_1.png')
+
+csv = np.insert(array,0,SAMPLE_RATE)
+np.savetxt('Project_Task_1.csv',csv,delimiter=',')
+
+plt.show()
+print("done")
