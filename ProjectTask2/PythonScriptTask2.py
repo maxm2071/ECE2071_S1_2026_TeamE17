a17259e6eb84ce231be4add5e31d303d45af1a16
maxm2071 <mall0045@student.monash.edu>
2026-05-05 15:44:01 +1000

    ALL WORKING TASK 1 FILES ARE IN PROJECTTASK1
    also added project task 2 folder, so add files to there

--- ProjectTask2/PythonScriptTask2.py
+++ ProjectTask2/PythonScriptTask2.py
@@ -10,30 +10,36 @@ devices = serial.tools.list_ports.comports()
 for i in devices:
     print(i)
 
-ser = serial.Serial("COM8", 921600)
+ser = serial.Serial("COM3", 115200)
 
 my_list = []
 
 SAMPLE_RATE = 22050
+"""
 US_sensor = input("do you want distance trigger mode (Y/N): ")
 if US_sensor == "Y":
     US_sensor = 1
-    
+
 else:
     US_sensor = 0
+"""
 
-ser.write(US_sensor.encode())
+US_sensor = 1
+# ser.write(US_sensor.encode())
 if US_sensor == 1:
-
     byte = ser.read(1)
+    print('hello')
     timeout = False
     while(timeout == False):
+        print('yes')
         bit = byte[0]
         my_list.append(bit)
         current_time = time.time()
         byte = ser.read(1)
-        if (time.time()-current_time > 0.5):
+
+        if (time.time() - current_time > 1):
             timeout = True
+            print('done')
 
 
 else:
@@ -47,7 +53,7 @@ else:
 
 
 data = np.array(my_list)
-print(len(data))
+print(data)
 
 data = (data - data.min()) / (data.max() - data.min())
 data = data * 255
@@ -58,7 +64,9 @@ with wave.open('Project_Task_1.wav', 'wb') as wf:
     wf.setsampwidth(1)
     wf.setframerate(SAMPLE_RATE)
     wf.writeframes(data.tobytes())
-array = np.arange(0, clip_length, 1 / SAMPLE_RATE)
+
+"""
+array = np.arange(0, len(data), 1 / SAMPLE_RATE)
 plt.plot(array, data)
 plt.ylabel('Amplitude')
 plt.xlabel('Time (s)')
@@ -70,5 +78,5 @@ np.savetxt('Project_Task_1.csv', csv, delimiter=',')
 
 plt.show()
 print("done")
-
+"""
 
